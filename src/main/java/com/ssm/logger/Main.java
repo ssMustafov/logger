e8200eda4c4f9180925ef5021967908d7ddbf8f5
Sinan Mustafov <ss.mustafov@gmail.com>
2019-09-22 23:23:29 +0300

    Improve the sample

--- src/main/java/com/ssm/logger/Main.java
+++ src/main/java/com/ssm/logger/Main.java
@@ -2,10 +2,13 @@ package com.ssm.logger;
 
 import com.ssm.logger.appenders.Appender;
 import com.ssm.logger.appenders.ConsoleAppender;
+import com.ssm.logger.appenders.FileAppender;
 import com.ssm.logger.formatters.Formatter;
+import com.ssm.logger.formatters.PatternFormatter;
 import com.ssm.logger.formatters.SimpleFormatter;
 
-import java.util.Collections;
+import java.io.File;
+import java.util.Arrays;
 
 /**
  * @author smustafov
@@ -14,10 +17,12 @@ public class Main {
 
     public static void main(String[] args) {
         Formatter simpleFormatter = new SimpleFormatter();
+        Formatter patternFormatter = new PatternFormatter("{date:yyyy/MM/dd HH:mm:ss,SSS} - {level} | {message}");
 
         Appender consoleAppender = new ConsoleAppender(simpleFormatter);
+        Appender fileAppender = new FileAppender(patternFormatter, new File("C:\\Users\\ssmus\\Desktop\\app.log"));
 
-        Logger logger = new Logger(Collections.singletonList(consoleAppender));
+        Logger logger = new Logger(Arrays.asList(consoleAppender, fileAppender));
         logger.info("Successfully released change log lock");
         logger.warn("Found more than one value for field [lastPublishedRevision]");
         logger.error("javax.resource.ResourceException: IJ000451: The connection manager is shutdown");
