3b457e820754fe244d1f782645e8489ae94f49f2
Sinan Mustafov <ss.mustafov@gmail.com>
2019-09-22 23:29:02 +0300

    Add comment for formatter interface

--- src/main/java/com/ssm/logger/formatters/Formatter.java
+++ src/main/java/com/ssm/logger/formatters/Formatter.java
@@ -3,6 +3,8 @@ package com.ssm.logger.formatters;
 import com.ssm.logger.context.LoggerContext;
 
 /**
+ * Used for formatting the log messages per {@link com.ssm.logger.appenders.Appender}.
+ *
  * @author smustafov
  */
 public interface Formatter {
